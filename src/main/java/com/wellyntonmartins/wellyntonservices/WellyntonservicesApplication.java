09211a96747e6e591ddcded061bdc091dd828e0a
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2026-02-03 10:24:58 -0300

    Project creation

---
+++ src/main/java/com/wellyntonmartins/wellyntonservices/WellyntonservicesApplication.java
@@ -0,0 +1,13 @@
+package com.wellyntonmartins.wellyntonservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellyntonservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellyntonservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellyntonmartins/wellyntonservices/WellyntonservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellyntonmartins.wellyntonservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellyntonservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
