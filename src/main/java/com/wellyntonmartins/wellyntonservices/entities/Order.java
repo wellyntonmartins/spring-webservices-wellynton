383b23e22d4f159a4df400d0d6a45a39ab93835d
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2026-02-04 15:56:23 -0300

    Added Order status

--- src/main/java/com/wellyntonmartins/wellyntonservices/entities/Order.java
+++ src/main/java/com/wellyntonmartins/wellyntonservices/entities/Order.java
@@ -1,6 +1,7 @@
 package com.wellyntonmartins.wellyntonservices.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wellyntonmartins.wellyntonservices.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne // Define que essa classe (Order) tem uma associacao de "muitos pra um" com a classe do atributo/objeto
     // User abaixo (foreign key)
@@ -26,9 +28,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +49,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
